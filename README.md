810469f953a2f3e71a5c9f4ac3ee0b67828368a9
Aytixel <aytixel@gmail.com>
2021-12-04 22:46:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Licence_Informatique_LeMans_App
+
+Application à destination de toute personne se trouvant en licence informatique à l'université du Mans.
---
+++ android/app/src/main/kotlin/com/example/licence_informatique_lemans_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.licence_informatique_lemans_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
